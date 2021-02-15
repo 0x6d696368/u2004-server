94f39ee8e3c2aa0a5d880bcefa6ba02df8fa2f63
0x6d696368 <>
2021-02-15 22:57:11 +0100

    Fixed X11 forwarding

--- 03_install_xforwarding.sh
+++ 03_install_xforwarding.sh
@@ -5,9 +5,9 @@ apt install -y xterm
 sed 's/X11Forwarding no/X11Forwarding yes/g' -i /etc/ssh/sshd_config
 
 cat >> /etc/ssh/sshd_config << EOF
-X11UseLocalhost no
+X11UseLocalhost yes
+AddressFamily inet
 EOF
 
 systemctl restart ssh
-
 
